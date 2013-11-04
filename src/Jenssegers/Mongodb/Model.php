257b92fe2b627c4ad268194e03b01a546ef1e54b
Jens Segers <segers.jens@gmail.com>
2013-11-04 17:03:30 +0100

    Updating model to 4.1 methods, check issue 63

--- src/Jenssegers/Mongodb/Model.php
+++ src/Jenssegers/Mongodb/Model.php
@@ -114,66 +114,82 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model {
     }
 
     /**
-     * Define a one-to-one relationship.
-     *
-     * @param  string  $related
-     * @param  string  $foreignKey
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function hasOne($related, $foreignKey = null)
+    * Define a one-to-one relationship.
+    *
+    * @param  string  $related
+    * @param  string  $foreignKey
+    * @param  string  $localKey
+    * @return \Illuminate\Database\Eloquent\Relations\HasOne
+    */
+    public function hasOne($related, $foreignKey = null, $localKey = null)
     {
         $foreignKey = $foreignKey ?: $this->getForeignKey();
 
         $instance = new $related;
 
-        return new HasOne($instance->newQuery(), $this, $foreignKey);
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new HasOne($instance->newQuery(), $this, $foreignKey, $localKey);
     }
 
     /**
-     * Define a one-to-many relationship.
-     *
-     * @param  string  $related
-     * @param  string  $foreignKey
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function hasMany($related, $foreignKey = null)
+    * Define a one-to-many relationship.
+    *
+    * @param  string  $related
+    * @param  string  $foreignKey
+    * @param  string  $localKey
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function hasMany($related, $foreignKey = null, $localKey = null)
     {
         $foreignKey = $foreignKey ?: $this->getForeignKey();
 
         $instance = new $related;
 
-        return new HasMany($instance->newQuery(), $this, $foreignKey);
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new HasMany($instance->newQuery(), $this, $foreignKey, $localKey);
     }
 
     /**
-     * Define an inverse one-to-one or many relationship.
-     *
-     * @param  string  $related
-     * @param  string  $foreignKey
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function belongsTo($related, $foreignKey = null)
+    * Define an inverse one-to-one or many relationship.
+    *
+    * @param  string  $related
+    * @param  string  $foreignKey
+    * @param  string  $otherKey
+    * @param  string  $relation
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function belongsTo($related, $foreignKey = null, $otherKey = null, $relation = null)
     {
-        list(, $caller) = debug_backtrace(false);
+        // If no relation name was given, we will use this debug backtrace to extract
+        // the calling method's name and use that as the relationship name as most
+        // of the time this will be what we desire to use for the relatinoships.
+        if (is_null($relation))
+        {
+            list(, $caller) = debug_backtrace(false);
+
+            $relation = $caller['function'];
+        }
 
         // If no foreign key was supplied, we can use a backtrace to guess the proper
         // foreign key name by using the name of the relationship function, which
         // when combined with an "_id" should conventionally match the columns.
-        $relation = $caller['function'];
-
         if (is_null($foreignKey))
         {
             $foreignKey = snake_case($relation).'_id';
         }
 
+        $instance = new $related;
+
         // Once we have the foreign key names, we'll just create a new Eloquent query
         // for the related models and returns the relationship instance which will
         // actually be responsible for retrieving and hydrating every relations.
-        $instance = new $related;
-
         $query = $instance->newQuery();
 
-        return new BelongsTo($query, $this, $foreignKey, $relation);
+        $otherKey = $otherKey ?: $instance->getKeyName();
+
+        return new BelongsTo($query, $this, $foreignKey, $otherKey, $relation);
     }
 
     /**
