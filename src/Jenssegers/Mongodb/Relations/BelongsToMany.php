dbd11e9ac71f85387ae80b07c3e155be7d283ad6
Jens Segers <segers.jens@gmail.com>
2013-11-20 22:43:43 +0100

    Tweaking BelongsToMany namespaces

--- src/Jenssegers/Mongodb/Relations/BelongsToMany.php
+++ src/Jenssegers/Mongodb/Relations/BelongsToMany.php
@@ -1,8 +1,7 @@
 <?php namespace Jenssegers\Mongodb\Relations;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany as EloquentBelongsToMany;
 
 class BelongsToMany extends EloquentBelongsToMany {
