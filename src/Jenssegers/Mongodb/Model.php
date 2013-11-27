c777e31a81a7b0d3b818c6d60af334376a44c30e
Jens Segers <segers.jens@gmail.com>
2013-11-27 11:45:05 +0100

    Updating BelongsToMany method to match 4.1, fixes #73

--- src/Jenssegers/Mongodb/Model.php
+++ src/Jenssegers/Mongodb/Model.php
@@ -201,32 +201,42 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model {
     }
 
 	/**
-	 * Define a many-to-many relationship.
-	 *
-	 * @param  string  $related
-	 * @param  string  $table
-	 * @param  string  $foreignKey
-	 * @param  string  $otherKey
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */
-	public function belongsToMany($related, $collection = null, $foreignKey = null, $otherKey = null)
+     * Define a many-to-many relationship.
+     *
+     * @param  string  $related
+     * @param  string  $collection
+     * @param  string  $foreignKey
+     * @param  string  $otherKey
+     * @param  string  $relation
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function belongsToMany($related, $collection = null, $foreignKey = null, $otherKey = null, $relation = null)
 	{
-		$caller = $this->getBelongsToManyCaller();
-		
+		// If no relationship name was passed, we will pull backtraces to get the
+        // name of the calling function. We will use that function name as the
+        // title of this relation since that is a great convention to apply.
+        if (is_null($relation))
+        {
+            $caller = $this->getBelongsToManyCaller();
+
+            $name = $caller['function'];
+        }
+
 		// First, we'll need to determine the foreign key and "other key" for the
 		// relationship. Once we have determined the keys we'll make the query
 		// instances as well as the relationship instances we need for this.
 		$foreignKey = $foreignKey ?: $this->getForeignKey() . 's';
 
 		$instance = new $related;
-		
+
 		$otherKey = $otherKey ?: $instance->getForeignKey() . 's';
+
 		// If no table name was provided, we can guess it by concatenating the two
 		// models using underscores in alphabetical order. The two model names
 		// are transformed to snake case from their default CamelCase also.
 		if (is_null($collection))
 		{
-			$collection = snake_case(str_plural(class_basename($related)));
+			$collection = $instance->getTable();
 		}
 
 		// Now we're ready to create a new query builder for the related model and
@@ -234,7 +244,7 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model {
 		// appropriate query constraint and entirely manages the hydrations.
 		$query = $instance->newQuery();
 
-		return new BelongsToMany($query, $this, $collection, $foreignKey, $otherKey, $caller['function']);
+		return new BelongsToMany($query, $this, $collection, $foreignKey, $otherKey, $relation);
 	}
 
     /**
