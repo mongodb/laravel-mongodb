96ebc95b781bfd87fc460ba767339b0b0bc21e98
Alexandre Gravel-Raymond <alexandre.gravel.raymond@concoursmania.com>
2014-04-16 17:40:05 +0200

    Add missing abstract methods from Illuminate\Auth\UserInterface in User test model

--- tests/models/User.php
+++ tests/models/User.php
@@ -85,6 +85,36 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
         return $this->email;
     }
 
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->rememberToken;
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->rememberToken = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName() {
+        return 'remember_token';
+    }
+
     protected function getDateFormat()
     {
         return 'l jS \of F Y h:i:s A';
