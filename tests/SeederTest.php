9eddaef5e0f3c183e05c2484af0d651d40b804a1
Jens Segers <segers.jens@gmail.com>
2014-08-10 13:30:59 +0200

    Add test for #280

--- tests/SeederTest.php
+++ tests/SeederTest.php
@@ -16,4 +16,12 @@ class SeederTest extends TestCase {
 		$this->assertTrue($user->seed);
 	}
 
+	public function testArtisan()
+	{
+		Artisan::call('db:seed');
+
+		$user = User::where('name', 'John Doe')->first();
+		$this->assertTrue($user->seed);
+	}
+
 }
