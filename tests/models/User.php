d1a212cd6b17e658e4eedba1097ac00c49409914
Jens Segers <segers.jens@gmail.com>
2014-09-16 13:02:31 +0200

    Update to L5

--- tests/models/User.php
+++ tests/models/User.php
@@ -3,11 +3,11 @@
 use Jenssegers\Mongodb\Model as Eloquent;
 
 use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Contracts\Auth\User as UserContract;
+use Illuminate\Contracts\Auth\Remindable as RemindableContract;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserContract, RemindableContract {
 
     use UserTrait, RemindableTrait;
 
