e7e6cb18088a8305235ae0e01fba6be147690a3c
Jens Segers <segers.jens@gmail.com>
2015-02-28 11:59:50 +0100

    Only log queries if logging is enabled

--- src/Jenssegers/Mongodb/Collection.php
+++ src/Jenssegers/Mongodb/Collection.php
@@ -38,34 +38,37 @@ class Collection {
      */
     public function __call($method, $parameters)
     {
-        $query = array();
-
-        // Build the query string.
-        foreach ($parameters as $parameter)
-        {
-            try
-            {
-                $query[] = json_encode($parameter);
-            }
-            catch (Exception $e)
-            {
-                $query[] = '{...}';
-            }
-        }
+        $query = [];
 
         $start = microtime(true);
 
         $result = call_user_func_array(array($this->collection, $method), $parameters);
 
-        // Once we have run the query we will calculate the time that it took to run and
-        // then log the query, bindings, and execution time so we will report them on
-        // the event that the developer needs them. We'll log time in milliseconds.
-        $time = $this->connection->getElapsedTime($start);
+        // Check if we need to log this query.
+        if ($this->connection->logging())
+        {
+            // Once we have run the query we will calculate the time that it took to run and
+            // then log the query, bindings, and execution time so we will report them on
+            // the event that the developer needs them. We'll log time in milliseconds.
+            $time = $this->connection->getElapsedTime($start);
 
-        // Convert the query to a readable string.
-        $queryString = $this->collection->getName() . '.' . $method . '(' . join(',', $query) . ')';
+            // Convert the query to a json string.
+            foreach ($parameters as $parameter)
+            {
+                try
+                {
+                    $query[] = json_encode($parameter);
+                }
+                catch (Exception $e)
+                {
+                    $query[] = '{...}';
+                }
+            }
 
-        $this->connection->logQuery($queryString, array(), $time);
+            $queryString = $this->collection->getName() . '.' . $method . '(' . join(',', $query) . ')';
+
+            $this->connection->logQuery($queryString, [], $time);
+        }
 
         return $result;
     }
