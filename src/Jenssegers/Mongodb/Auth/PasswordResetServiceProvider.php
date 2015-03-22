2d2704c6c234defc53b56cff839127228c1ccb24
Jens Segers <segers.jens@gmail.com>
2015-03-22 22:12:30 +0100

    Dump tests/bootstrap.php

--- src/Jenssegers/Mongodb/Auth/PasswordResetServiceProvider.php
+++ src/Jenssegers/Mongodb/Auth/PasswordResetServiceProvider.php
@@ -14,12 +14,14 @@ class PasswordResetServiceProvider extends \Illuminate\Auth\Passwords\PasswordRe
 		$this->app->singleton('auth.password.tokens', function($app)
 		{
 			$connection = $app['db']->connection();
+
 			// The database token repository is an implementation of the token repository
 			// interface, and is responsible for the actual storing of auth tokens and
 			// their e-mail addresses. We will inject this table and hash key to it.
 			$table = $app['config']['auth.password.table'];
 			$key = $app['config']['app.key'];
 			$expire = $app['config']->get('auth.password.expire', 60);
+
 			return new DbRepository($connection, $table, $key, $expire);
 		});
 	}
