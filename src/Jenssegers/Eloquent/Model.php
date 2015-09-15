e30243d8bb2fc2a0fb858926c152f7540793cf3b
Jens Segers <segers.jens@gmail.com>
2015-09-15 10:03:14 +0200

    Revert last commit

--- src/Jenssegers/Eloquent/Model.php
+++ src/Jenssegers/Eloquent/Model.php
@@ -7,22 +7,4 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model {
 
     use HybridRelations;
 
-    /**
-     * Get a new query builder instance for the connection.
-     *
-     * @return Builder
-     */
-    protected function newBaseQueryBuilder()
-    {
-        $connection = $this->getConnection();
-
-        // Check the connection type
-        if ($connection instanceof \Jenssegers\Mongodb\Connection)
-        {
-            return new QueryBuilder($connection, $connection->getPostProcessor());
-        }
-
-        return parent::newBaseQueryBuilder();
-    }
-
 }
