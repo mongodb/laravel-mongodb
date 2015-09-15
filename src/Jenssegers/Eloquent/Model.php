226af8b29a3721ca9439651733be1f47c4b4bae6
Jens Segers <segers.jens@gmail.com>
2015-09-15 09:27:56 +0200

    Add missing namespace

--- src/Jenssegers/Eloquent/Model.php
+++ src/Jenssegers/Eloquent/Model.php
@@ -1,6 +1,7 @@
 <?php namespace Jenssegers\Eloquent;
 
 use Jenssegers\Mongodb\Eloquent\HybridRelations;
+use Jenssegers\Mongodb\Query\Builder as QueryBuilder;
 
 abstract class Model extends \Illuminate\Database\Eloquent\Model {
 
