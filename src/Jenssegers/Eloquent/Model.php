d9760faaa5d4a70247350657f9f2320fdce44064
Jens Segers <segers.jens@gmail.com>
2015-09-15 10:03:26 +0200

    Revert last commit

--- src/Jenssegers/Eloquent/Model.php
+++ src/Jenssegers/Eloquent/Model.php
@@ -1,7 +1,6 @@
 <?php namespace Jenssegers\Eloquent;
 
 use Jenssegers\Mongodb\Eloquent\HybridRelations;
-use Jenssegers\Mongodb\Query\Builder as QueryBuilder;
 
 abstract class Model extends \Illuminate\Database\Eloquent\Model {
 
