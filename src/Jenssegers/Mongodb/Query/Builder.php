bcb22582854240fbaf58e6a27f79de0b8f13af4d
Robert-John van Doesburg <rjvandoesburg@hotmail.com>
2016-03-10 14:29:07 +0100

    Allow pluck to work with _id
    - As of 5.2 lists is deprecated so I moved the ObjectID to string code to pluck as well
      I kept pluck the same for backwards compatibility

--- src/Jenssegers/Mongodb/Query/Builder.php
+++ src/Jenssegers/Mongodb/Query/Builder.php
@@ -566,6 +566,19 @@ class Builder extends BaseBuilder
      */
     public function pluck($column, $key = null)
     {
+        if ($key == '_id') {
+            $results = new Collection($this->get([$column, $key]));
+
+            // Convert ObjectID's to strings so that lists can do its work.
+            $results = $results->map(function ($item) {
+                $item['_id'] = (string) $item['_id'];
+
+                return $item;
+            });
+
+            return $results->pluck($column, $key)->all();
+        }
+
         $results = $this->get(is_null($key) ? [$column] : [$column, $key]);
 
         // If the columns are qualified with a table or have an alias, we cannot use
