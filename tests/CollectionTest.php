37398d2ef30064042fc80c00f546191aa9b7ccb9
Renato Moura <renato@naturalweb.com.br>
2016-03-30 16:44:25 -0300

    Fixed logQuery to display ObjectID in parameters

---
+++ tests/CollectionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Jenssegers\Mongodb\Connection;
+use Jenssegers\Mongodb\Collection;
+use MongoDB\Collection as MongoCollection;
+use MongoDB\BSON\ObjectID;
+
+class CollectionTest extends TestCase
+{
+
+    public function testExecuteMethodCall()
+    {
+        $return = ['foo' => 'bar'];
+        $where = ['id' => new ObjectID('56f94800911dcc276b5723dd')];
+        $time = 1.1;
+        $queryString = 'name-collection.findOne({"id":"56f94800911dcc276b5723dd"})';
+
+        $mongoCollection = $this->getMockBuilder(MongoCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mongoCollection->expects($this->once())->method('findOne')->with($where)->willReturn($return);
+        $mongoCollection->expects($this->once())->method('getCollectionName')->willReturn('name-collection');
+
+        $connection = $this->getMockBuilder(Connection::class)->disableOriginalConstructor()->getMock();
+        $connection->expects($this->once())->method('logging')->willReturn(true);
+        $connection->expects($this->once())->method('getElapsedTime')->willReturn($time);
+        $connection->expects($this->once())->method('logQuery')->with($queryString, [], $time);
+        
+        $collection = new Collection($connection, $mongoCollection);
+
+        $this->assertEquals($return, $collection->findOne($where));
+    }
+}
