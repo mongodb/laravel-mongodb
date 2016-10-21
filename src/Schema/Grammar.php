46a8003c206f81c614417d6e1c14bd9b116976ce
Michael Lühr <michael.luehr@gmail.com>
2016-10-21 13:37:02 +0200

    introduce and use schema grammar (fixes #33)

---
+++ src/Schema/Grammar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Moloquent\Schema;
+
+use Illuminate\Database\Schema\Grammars\Grammar as BaseGrammar;
+
+/**
+ * Class Grammar
+ *
+ * @package Moloquent\Schema
+ */
+class Grammar extends BaseGrammar
+{
+}
