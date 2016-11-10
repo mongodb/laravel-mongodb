6ffef4ba3f50f34079f8a2b303303e22dd4248b8
Fahmi Ardi <f4hem.net@gmail.com>
2016-11-10 21:22:51 +0700

    add connection config for reset password

--- src/Jenssegers/Mongodb/Auth/PasswordBrokerManager.php
+++ src/Jenssegers/Mongodb/Auth/PasswordBrokerManager.php
@@ -12,8 +12,10 @@ class PasswordBrokerManager extends BasePasswordBrokerManager
      */
     protected function createTokenRepository(array $config)
     {
+        $connection = isset($config['connection']) ? $config['connection'] : null;
+
         return new DatabaseTokenRepository(
-            $this->app['db']->connection(),
+            $this->app['db']->connection($connection),
             $config['table'],
             $this->app['config']['app.key'],
             $config['expire']
