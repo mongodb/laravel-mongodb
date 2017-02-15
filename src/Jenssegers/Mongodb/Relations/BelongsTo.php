ef8596a5be8586929b25002a3a40f920582f0e46
Eslam Salem <netcodez@gmail.com>
2017-02-15 13:46:32 +0200

    get the Other/Owner Key name based on different version of Illuminate/Database

--- src/Jenssegers/Mongodb/Relations/BelongsTo.php
+++ src/Jenssegers/Mongodb/Relations/BelongsTo.php
@@ -11,7 +11,7 @@ class BelongsTo extends \Illuminate\Database\Eloquent\Relations\BelongsTo
             // For belongs to relationships, which are essentially the inverse of has one
             // or has many relationships, we need to actually query on the primary key
             // of the related models matching on the foreign key that's on a parent.
-            $this->query->where($this->otherKey, '=', $this->parent->{$this->foreignKey});
+            $this->query->where($this->getOtherKeyPropertyName(), '=', $this->parent->{$this->foreignKey});
         }
     }
 
@@ -25,8 +25,18 @@ class BelongsTo extends \Illuminate\Database\Eloquent\Relations\BelongsTo
         // We'll grab the primary key name of the related models since it could be set to
         // a non-standard name and not "id". We will then construct the constraint for
         // our eagerly loading query so it returns the proper models from execution.
-        $key = $this->otherKey;
+        $key = $this->getOtherKeyPropertyName();
 
         $this->query->whereIn($key, $this->getEagerModelKeys($models));
+    }
+
+    /**
+     * get the Other/Owner Key name based on different version of Illuminate/Database
+     * see commit https://github.com/illuminate/database/commit/6a35698d72e276f435324b7e29b3cd37ef7d5d9c
+     * @return string
+     */
+    public function getOtherKeyPropertyName()
+    {
+        return property_exists($this, "ownerKey") ? $this->ownerKey : $this->otherKey;
     }
 }
