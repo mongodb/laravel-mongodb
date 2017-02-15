21a13146e167064ecd2f9c08aead0231d5d1cebb
Dariusz Prząda <artdarek@gmail.com>
2017-02-15 16:54:29 +0100

    Authenticatable User Class Added (#1118)

---
+++ src/Jenssegers/Mongodb/Auth/User.php
@@ -0,0 +1,17 @@
+<?php namespace Jenssegers\Mongodb\Auth;
+
+use Illuminate\Auth\Authenticatable;
+use Jenssegers\Mongodb\Eloquent\Model as Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword;
+}
