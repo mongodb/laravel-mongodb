338666bc57bdd6fe1c251375590284464f691acb
Joey Houtenbos <joey@7lab.nl>
2017-03-30 13:46:32 +0200

    Add missing method to get belongsToMany-caller Replace "otherKey" by "relatedKey"

--- src/Eloquent/HybridRelations.php
+++ src/Eloquent/HybridRelations.php
@@ -262,4 +262,22 @@ trait HybridRelations
 
         return new BelongsToMany($query, $this, $collection, $foreignKey, $otherKey, $relation);
     }
+
+    /**
+     * Get the relationship name of the belongs to many.
+     *
+     * @return string
+     */
+    protected function getBelongsToManyCaller()
+    {
+        $self = __FUNCTION__;
+
+        $caller = array_first(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), function ($trace) use ($self) {
+            $caller = $trace['function'];
+
+            return ! in_array($caller, Model::$manyMethods) && $caller != $self;
+        });
+
+        return ! is_null($caller) ? $caller['function'] : null;
+    }
 }
