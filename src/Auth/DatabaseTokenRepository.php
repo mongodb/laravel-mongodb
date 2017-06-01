3c562a69edf8da1d8ee38ca8066d5c5a980c25b5
Thiago Carnaes <thiago.carnaes@gmail.com>
2017-06-01 15:06:38 -0300

    Update DatabaseTokenRepository.php
    Update in function getPayload in created_at 
    Update in function tokenExpired, change the variable $token to $createdAt

--- src/Auth/DatabaseTokenRepository.php
+++ src/Auth/DatabaseTokenRepository.php
@@ -19,29 +19,29 @@ class DatabaseTokenRepository extends BaseDatabaseTokenRepository
      */
     protected function getPayload($email, $token)
     {
-        return ['email' => $email, 'token' => $token, 'created_at' => new UTCDateTime(round(microtime(true) * 1000))];
+        return ['email' => $email, 'token' => $this->hasher->make($token), 'created_at' => new UTCDateTime(time() * 1000)];
     }
 
     /**
      * Determine if the token has expired.
      *
-     * @param array $token
+     * @param array $createdAt
      *
      * @return bool
      */
-    protected function tokenExpired($token)
+    protected function tokenExpired($createdAt)
     {
         // Convert UTCDateTime to a date string.
-        if ($token['created_at'] instanceof UTCDateTime) {
-            $date = $token['created_at']->toDateTime();
+        if ($createdAt instanceof UTCDateTime) {
+            $date = $createdAt->toDateTime();
             $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
-            $token['created_at'] = $date->format('Y-m-d H:i:s');
-        } elseif (is_array($token['created_at']) and isset($token['created_at']['date'])) {
-            $date = new DateTime($token['created_at']['date'], new DateTimeZone(isset($token['created_at']['timezone']) ? $token['created_at']['timezone'] : 'UTC'));
+            $createdAt = $date->format('Y-m-d H:i:s');
+        } elseif (is_array($createdAt) and isset($createdAt['date'])) {
+            $date = new DateTime($createdAt['date'], new DateTimeZone(isset($createdAt['timezone']) ? $createdAt['timezone'] : 'UTC'));
             $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
-            $token['created_at'] = $date->format('Y-m-d H:i:s');
+            $createdAt = $date->format('Y-m-d H:i:s');
         }
 
-        return parent::tokenExpired($token);
+        return parent::tokenExpired($createdAt);
     }
 }
