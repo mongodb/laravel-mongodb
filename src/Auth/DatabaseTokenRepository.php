e5ae4baa82ae0de94561271e16c936d03439d7b9
Thiago Carnaes <thiago.carnaes@gmail.com>
2017-06-01 15:55:22 -0300

    Update DatabaseTokenRepository.php
    Change the the function getPayload and tokenExpired

--- src/Auth/DatabaseTokenRepository.php
+++ src/Auth/DatabaseTokenRepository.php
@@ -2,10 +2,10 @@
 
 namespace Moloquent\Auth;
 
+use Carbon\Carbon;
 use DateTime;
 use DateTimeZone;
 use Illuminate\Auth\Passwords\DatabaseTokenRepository as BaseDatabaseTokenRepository;
-use MongoDB\BSON\UTCDateTime;
 
 class DatabaseTokenRepository extends BaseDatabaseTokenRepository
 {
@@ -19,7 +19,7 @@ class DatabaseTokenRepository extends BaseDatabaseTokenRepository
      */
     protected function getPayload($email, $token)
     {
-        return ['email' => $email, 'token' => $this->hasher->make($token), 'created_at' => new UTCDateTime(time() * 1000)];
+        return ['email' => $email, 'token' => $this->hasher->make($token), 'created_at' => new Carbon];
     }
 
     /**
@@ -31,17 +31,6 @@ class DatabaseTokenRepository extends BaseDatabaseTokenRepository
      */
     protected function tokenExpired($createdAt)
     {
-        // Convert UTCDateTime to a date string.
-        if ($createdAt instanceof UTCDateTime) {
-            $date = $createdAt->toDateTime();
-            $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
-            $createdAt = $date->format('Y-m-d H:i:s');
-        } elseif (is_array($createdAt) and isset($createdAt['date'])) {
-            $date = new DateTime($createdAt['date'], new DateTimeZone(isset($createdAt['timezone']) ? $createdAt['timezone'] : 'UTC'));
-            $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
-            $createdAt = $date->format('Y-m-d H:i:s');
-        }
-
-        return parent::tokenExpired($createdAt);
+        return Carbon::parse($createdAt['date'])->addSeconds($this->expires)->isPast();
     }
 }
