4868a7040670fbc53593d58310fa85bcd1c4f4ce
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-02-16 23:42:25 +1100

    Updated to create build folder for code cove

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,6 @@
 sleep 3 &&
 composer install --prefer-source --no-interaction &&
 php ./vendor/bin/phpunit &&
-php ./vendor/bin/phpunit --coverage-clover build/logs/clover.xml &&
+mkdir -p ./build/logs &&
+php ./vendor/bin/phpunit --coverage-clover ./build/logs/clover.xml &&
 php ./vendor/bin/php-coverall -v
