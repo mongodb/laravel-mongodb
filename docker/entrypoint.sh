e8a40cc6a1e281e5621fa7f0876fd4c7c657a073
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-02-17 01:16:54 +1100

    Without code-cove

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-sleep 3 &&
-composer install --prefer-source --no-interaction &&
-php ./vendor/bin/phpunit &&
-mkdir -p ./build/logs &&
-php ./vendor/bin/phpunit --coverage-clover ./build/logs/clover.xml &&
-php ./vendor/bin/php-coverall -v
+sleep 3 && composer install --prefer-source --no-interaction && php ./vendor/bin/phpunit
