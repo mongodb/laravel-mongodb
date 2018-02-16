aa2a6e002b587e170912afc7645f3338466de235
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-02-16 23:03:51 +1100

    Updated to shorten the command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-sleep 3 && composer install --prefer-source --no-interaction && php ./vendor/bin/phpunit
+sleep 3 &&
+composer install --prefer-source --no-interaction &&
+php ./vendor/bin/phpunit &&
+php ./vendor/bin/phpunit --coverage-clover build/logs/clover.xml &&
+php ./vendor/bin/coveralls -v
