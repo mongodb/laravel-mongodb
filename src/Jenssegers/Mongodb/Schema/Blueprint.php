bb3f71e35a98a00a0f1cdb31074b00b5eb8547f7
Stanislav Shupilkin <sshupilkin@virtuman.com>
2018-09-26 08:44:40 +0300

    Add parameter options for creating collection

--- src/Jenssegers/Mongodb/Schema/Blueprint.php
+++ src/Jenssegers/Mongodb/Schema/Blueprint.php
@@ -188,14 +188,14 @@ class Blueprint extends \Illuminate\Database\Schema\Blueprint
     /**
      * @inheritdoc
      */
-    public function create()
+    public function create(array $options = [])
     {
         $collection = $this->collection->getCollectionName();
 
         $db = $this->connection->getMongoDB();
 
         // Ensure the collection is created.
-        $db->createCollection($collection);
+        $db->createCollection($collection, $options);
     }
 
     /**
