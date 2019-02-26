08868a2a716711dcd992913118440d8fd63f0547
Filip Iulian Pacurar <hello@filipac.net>
2019-02-26 19:40:34 +0200

    Update Model.php

--- src/Jenssegers/Mongodb/Eloquent/Model.php
+++ src/Jenssegers/Mongodb/Eloquent/Model.php
@@ -228,7 +228,7 @@ abstract class Model extends BaseModel
     /**
      * @inheritdoc
      */
-    protected function originalIsEquivalent($key, $current)
+    public function originalIsEquivalent($key, $current)
     {
         if (!array_key_exists($key, $this->original)) {
             return false;
