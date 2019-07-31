0bde2e703e712ee171e7bb3b8b8d10b6814694d1
Stanislav Shupilkin <smolevich90@gmail.com>
2019-07-31 22:57:19 +0300

    Fix format in Model

--- src/Jenssegers/Mongodb/Eloquent/Model.php
+++ src/Jenssegers/Mongodb/Eloquent/Model.php
@@ -175,7 +175,7 @@ abstract class Model extends BaseModel
     public function setAttribute($key, $value)
     {
         // Convert _id to ObjectID.
-        if ($key == '_id'  && is_string($value)) {
+        if ($key == '_id' && is_string($value)) {
             $builder = $this->newBaseQueryBuilder();
 
             $value = $builder->convertKey($value);
