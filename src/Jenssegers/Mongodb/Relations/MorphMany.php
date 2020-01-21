92851a2b59b9b0fce85a31780d1e766275878957
Majed6 <7234719+Majed6@users.noreply.github.com>
2020-01-21 07:52:41 +0300

    Add style fixes
    (cherry picked from commit 1115bf5e545f2edc06261336aefbd1e1c5d188fc)

--- src/Jenssegers/Mongodb/Relations/MorphMany.php
+++ src/Jenssegers/Mongodb/Relations/MorphMany.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Relations\MorphMany as EloquentMorphMany;
 
 class MorphMany extends EloquentMorphMany
 {
-  /**
-   * Get the name of the "where in" method for eager loading.
-   *
-   * @param \Illuminate\Database\Eloquent\Model $model
-   * @param string $key
-   *
-   * @return string
-   */
-  protected function whereInMethod( EloquentModel $model, $key )
-  {
-    return 'whereIn';
-  }
+    /**
+     * Get the name of the "where in" method for eager loading.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $key
+     *
+     * @return string
+     */
+    protected function whereInMethod(EloquentModel $model, $key)
+    {
+        return 'whereIn';
+    }
 }
