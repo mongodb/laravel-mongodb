a78d7ba0a7a091d07fd0e3daac8e4b8a30af2d4f
Stephan de Souza <blad3d@gmail.com>
2020-03-11 19:51:11 -0300

    Removing unnecessary dockblocks

--- src/Jenssegers/Mongodb/Relations/EmbedsOne.php
+++ src/Jenssegers/Mongodb/Relations/EmbedsOne.php
@@ -8,9 +8,7 @@ use MongoDB\BSON\ObjectID;
 
 class EmbedsOne extends EmbedsOneOrMany
 {
-    /**
-     * @inheritdoc
-     */
+
     public function initRelation(array $models, $relation)
     {
         foreach ($models as $model) {
@@ -20,17 +18,11 @@ class EmbedsOne extends EmbedsOneOrMany
         return $models;
     }
 
-    /**
-     * @inheritdoc
-     */
     public function getResults()
     {
         return $this->toModel($this->getEmbedded());
     }
 
-    /**
-     * @inheritdoc
-     */
     public function getEager()
     {
         $eager = $this->get();
