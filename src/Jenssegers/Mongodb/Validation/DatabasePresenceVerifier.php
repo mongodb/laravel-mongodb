4f3f0b838beebefa7af000ab071fb88d3f543728
divine <48183131+divine@users.noreply.github.com>
2020-12-18 02:57:47 +0300

    Backport of #1992 to 3.7
    Add escape regex chars to DB Presence Verifier [#1992](https://github.com/jenssegers/laravel-mongodb/pull/1992) by [@andrei-gafton-rtgt](https://github.com/andrei-gafton-rtgt).
    
    Co-Authored-By: andrei-gafton-rtgt <61140282+andrei-gafton-rtgt@users.noreply.github.com>

--- src/Jenssegers/Mongodb/Validation/DatabasePresenceVerifier.php
+++ src/Jenssegers/Mongodb/Validation/DatabasePresenceVerifier.php
@@ -16,7 +16,7 @@ class DatabasePresenceVerifier extends \Illuminate\Validation\DatabasePresenceVe
      */
     public function getCount($collection, $column, $value, $excludeId = null, $idColumn = null, array $extra = [])
     {
-        $query = $this->table($collection)->where($column, 'regex', "/$value/i");
+        $query = $this->table($collection)->where($column, 'regex', "/" . preg_quote($value) . "/i");
 
         if ($excludeId !== null && $excludeId != 'NULL') {
             $query->where($idColumn ?: 'id', '<>', $excludeId);
