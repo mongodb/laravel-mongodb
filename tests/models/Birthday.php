21c89ea4ef81179f4a5287f148f5b080023e64eb
Yexk <yexk@yexk.cn>
2021-05-12 11:07:40 +0800

    add test and doc

---
+++ tests/models/Birthday.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+/**
+ * Class Birthday.
+ * @property string $name
+ * @property string $birthday
+ * @property string $day
+ * @property string $month
+ * @property string $year
+ * @property string $time
+ */
+class Birthday extends Eloquent
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'birthday';
+    protected $fillable = ['name', 'birthday', 'day', 'month', 'year', 'time'];
+}
