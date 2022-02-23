19eb13b71628dbda71f65c24564321c10c603359
Rob Brain <robjbrain@gmail.com>
2022-02-23 18:17:41 +1200

    Check if failed log storage is disabled in Queue Service Provider
    Fixes this issue: https://github.com/jenssegers/laravel-mongodb/issues/2356

--- src/MongodbQueueServiceProvider.php
+++ src/MongodbQueueServiceProvider.php
@@ -13,7 +13,7 @@ class MongodbQueueServiceProvider extends QueueServiceProvider
     protected function registerFailedJobServices()
     {
         // Add compatible queue failer if mongodb is configured.
-        if ($this->app['db']->connection(config('queue.failed.database'))->getDriverName() == 'mongodb') {
+        if (config('queue.failed.driver') !== null && $this->app['db']->connection(config('queue.failed.database'))->getDriverName() == 'mongodb') {
             $this->app->singleton('queue.failer', function ($app) {
                 return new MongoFailedJobProvider($app['db'], config('queue.failed.database'), config('queue.failed.table'));
             });
