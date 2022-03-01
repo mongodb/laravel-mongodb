5813e6c7dd32c279706348348d352b434cbdce6f
Rob Brain <robjbrain@gmail.com>
2022-03-01 15:25:30 +0700

    Update MongodbQueueServiceProvider.php
    Update to use Illuminate\Queue\Failed\NullFailedJobProvider

--- src/MongodbQueueServiceProvider.php
+++ src/MongodbQueueServiceProvider.php
@@ -4,6 +4,7 @@ namespace Jenssegers\Mongodb;
 
 use Illuminate\Queue\QueueServiceProvider;
 use Jenssegers\Mongodb\Queue\Failed\MongoFailedJobProvider;
+use Illuminate\Queue\Failed\NullFailedJobProvider;
 
 class MongodbQueueServiceProvider extends QueueServiceProvider
 {
@@ -12,8 +13,13 @@ class MongodbQueueServiceProvider extends QueueServiceProvider
      */
     protected function registerFailedJobServices()
     {
+        if (array_key_exists('driver', $config) &&
+            (is_null($config['driver']) || $config['driver'] === 'null')) {
+            return new NullFailedJobProvider;
+        }
+        
         // Add compatible queue failer if mongodb is configured.
-        if (config('queue.failed.driver') !== null && $this->app['db']->connection(config('queue.failed.database'))->getDriverName() == 'mongodb') {
+        if ($this->app['db']->connection(config('queue.failed.database'))->getDriverName() == 'mongodb') {
             $this->app->singleton('queue.failer', function ($app) {
                 return new MongoFailedJobProvider($app['db'], config('queue.failed.database'), config('queue.failed.table'));
             });
