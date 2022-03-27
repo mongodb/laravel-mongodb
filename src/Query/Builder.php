f942ad810e06e91833515877ada451a6de32fd58
David Surya <suryadavid@ymail.com>
2022-03-27 21:25:30 +0700

    (+) fix whereDay and whereMonth

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -1231,6 +1231,7 @@ class Builder extends BaseBuilder
         extract($where);
 
         $operator = $operator === '=' ? '$eq' : $this->conversion[$operator];
+        $value = str_starts_with($value, '0') ? intval(str_replace('0', '', $value)) : $value;
 
         return [
             '$expr' => [
@@ -1253,6 +1254,7 @@ class Builder extends BaseBuilder
         extract($where);
         
         $operator = $operator === '=' ? '$eq' : $this->conversion[$operator];
+        $value = str_starts_with($value, '0') ? intval(str_replace('0', '', $value)) : $value;
 
         return [
             '$expr' => [
