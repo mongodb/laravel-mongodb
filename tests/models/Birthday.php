9e8a63ebc734bbad4586d389142d79d8429eca0e
David <suryadavid@ymail.com>
2022-03-27 18:13:08 +0700

    (+) remove unused property

--- tests/models/Birthday.php
+++ tests/models/Birthday.php
@@ -9,14 +9,10 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
  *
  * @property string $name
  * @property string $birthday
- * @property string $day
- * @property string $month
- * @property string $year
- * @property string $time
  */
 class Birthday extends Eloquent
 {
     protected $connection = 'mongodb';
     protected $collection = 'birthday';
-    protected $fillable = ['name', 'birthday', 'day', 'month', 'year', 'time'];
+    protected $fillable = ['name', 'birthday'];
 }
