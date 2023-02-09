ba66a4ef8757095127ce9fc27b1686eb07d34575
Andreas Braun <git@alcaeus.org>
2023-02-09 09:47:26 +0100

    Fix auth test for Laravel 10

--- src/Auth/PasswordResetServiceProvider.php
+++ src/Auth/PasswordResetServiceProvider.php
@@ -7,29 +7,6 @@ use Illuminate\Auth\Passwords\PasswordResetServiceProvider as BasePasswordResetS
 class PasswordResetServiceProvider extends BasePasswordResetServiceProvider
 {
     /**
-     * Register the token repository implementation.
-     *
-     * @return void
-     */
-    protected function registerTokenRepository()
-    {
-        $this->app->singleton('auth.password.tokens', function ($app) {
-            $connection = $app['db']->connection();
-
-            // The database token repository is an implementation of the token repository
-            // interface, and is responsible for the actual storing of auth tokens and
-            // their e-mail addresses. We will inject this table and hash key to it.
-            $table = $app['config']['auth.password.table'];
-
-            $key = $app['config']['app.key'];
-
-            $expire = $app['config']->get('auth.password.expire', 60);
-
-            return new DatabaseTokenRepository($connection, $table, $key, $expire);
-        });
-    }
-
-    /**
      * @inheritdoc
      */
     protected function registerPasswordBroker()
