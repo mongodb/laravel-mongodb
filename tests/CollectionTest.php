76346d43db03ab44b13072f8b954e8302f9b014c
AlexNesvat <alexander.nesvat@element451.com>
2023-06-02 14:56:14 +0300

    Rework retry test and extend report message to be more informative

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -39,17 +39,28 @@ class CollectionTest extends TestCase
         $time = 1.1;
         $queryString = 'name-collection.findOne({"id":"56f94800911dcc276b5723dd"})';
 
-        $mongoCollection= m::mock(MongoCollection::class);
+        $mockFactory = $this->createMock(MongoCollection::class);
+        $matcher     = $this->exactly(2);
+        $mockFactory
+            ->expects($matcher)
+            ->method('findOne')
+            ->willReturnCallback(function () use ($matcher, $return) {
+                if ($matcher->getInvocationCount() === 1) {
+                    throw new Exception();
+                }
+                return $return;
+            });
 
         $connection = $this->getMockBuilder(Connection::class)->disableOriginalConstructor()->getMock();
+        $collection = new Collection($connection, $mockFactory);
 
-        $collection = new Collection($connection, $mongoCollection);
-        $this->expectException(Exception::class);
         $result = $collection->findOne($where);
 
         $connection->expects($this->once())->method('getElapsedTime')->willReturn($time);
-        $connection->expects($this->once())->method('logQuery')->with($queryString, [], $time);
+        $connection->getElapsedTime(123);
 
+        $connection->expects($this->once())->method('logQuery')->with($queryString, [], $time);
+        $connection->logQuery($queryString, [], $time);
 
         $this->assertEquals($return, $result);
     }
