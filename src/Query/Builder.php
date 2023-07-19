cf103ba0ddc8a4ca601aab338e8298e0fdbff018
Jérôme Tamarelle <jerome@tamarelle.net>
2023-07-19 10:39:18 +0200

    PHPORM-47 Improve Builder::whereBetween to support CarbonPeriod and reject invalid array (#10)
    The Query\Builder::whereBetween() method can be used like this:
    
    whereBetween('date_field', [min, max])
    whereBetween('date_field', collect([min, max]))
    whereBetween('date_field', CarbonPeriod)
    
    Laravel allows other formats: the $values array is flatten and the builder assumes there are at least 2 elements and ignore the others. It's a design that can lead to misunderstandings. I prefer to raise an exception when we have incorrect values, rather than trying to guess what the developer would like to do.
    
    Support for CarbonPeriod was fixed in Laravel 10: laravel/framework#46720 because the query builder was taking the 1st 2 values of the iterator instead of the start & end dates.

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -2,6 +2,7 @@
 
 namespace Jenssegers\Mongodb\Query;
 
+use Carbon\CarbonPeriod;
 use Closure;
 use DateTimeInterface;
 use Illuminate\Database\Query\Builder as BaseBuilder;
@@ -554,10 +555,19 @@ class Builder extends BaseBuilder
 
     /**
      * @inheritdoc
+     * @param list{mixed, mixed}|CarbonPeriod $values
      */
     public function whereBetween($column, iterable $values, $boolean = 'and', $not = false)
     {
         $type = 'between';
+
+        if ($values instanceof Collection) {
+            $values = $values->all();
+        }
+
+        if (is_array($values) && (! array_is_list($values) || count($values) !== 2)) {
+            throw new \InvalidArgumentException('Between $values must be a list with exactly two elements: [min, max]');
+        }
 
         $this->wheres[] = compact('column', 'type', 'boolean', 'values', 'not');
 
@@ -995,11 +1005,18 @@ class Builder extends BaseBuilder
                     }
                 }
             } elseif (isset($where['values'])) {
-                array_walk_recursive($where['values'], function (&$item, $key) {
-                    if ($item instanceof DateTimeInterface) {
-                        $item = new UTCDateTime($item);
-                    }
-                });
+                if (is_array($where['values'])) {
+                    array_walk_recursive($where['values'], function (&$item, $key) {
+                        if ($item instanceof DateTimeInterface) {
+                            $item = new UTCDateTime($item);
+                        }
+                    });
+                } elseif ($where['values'] instanceof CarbonPeriod) {
+                    $where['values'] = [
+                        new UTCDateTime($where['values']->getStartDate()),
+                        new UTCDateTime($where['values']->getEndDate()),
+                    ];
+                }
             }
 
             // The next item in a "chain" of wheres devices the boolean of the
