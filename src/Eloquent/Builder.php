348f0c1bace83e6eb6f54b39127889211406bef5
Jérôme Tamarelle <jerome@tamarelle.net>
2023-08-21 11:53:15 +0200

    Remove Eloquent\Builder::chunkById() already having the correct id field in laravel

--- src/Eloquent/Builder.php
+++ src/Eloquent/Builder.php
@@ -156,14 +156,6 @@ class Builder extends EloquentBuilder
     /**
      * @inheritdoc
      */
-    public function chunkById($count, callable $callback, $column = '_id', $alias = null)
-    {
-        return parent::chunkById($count, $callback, $column, $alias);
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function raw($expression = null)
     {
         // Get raw results from the query builder.
