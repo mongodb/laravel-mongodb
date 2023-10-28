b35c32d85e4023df0b15df162158a4497eb53b76
Mohammad Mortazavi <s.mohammad027@live.com>
2023-10-28 21:05:21 +0330

    Tests for object;

---
+++ tests/Casts/ObjectTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MongoDB\Laravel\Tests\Casts;
+
+use MongoDB\Laravel\Tests\Models\Casting;
+use MongoDB\Laravel\Tests\TestCase;
+
+class ObjectTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Casting::truncate();
+    }
+
+    public function testObject(): void
+    {
+        $model = Casting::query()->create(['objectValue' => ['g' => 'G-Eazy']]);
+        $check = Casting::query()->find($model->_id);
+
+        self::assertIsObject($check->objectValue);
+        self::assertEquals((object)['g' => 'G-Eazy'],$check->objectValue);
+
+        $model->update(['objectValue' => ['Dont let me go' => 'Even the brightest of colors turn greys']]);
+        $check = Casting::query()->find($model->_id);
+
+        self::assertIsObject($check->objectValue);
+        self::assertEquals((object)['Dont let me go' => 'Even the brightest of colors turn greys'],$check->objectValue);
+    }
+}
