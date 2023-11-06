3728b1f1b94f91453ab22077d895c96d061402e9
Mohammad Mortazavi <s.mohammad027@live.com>
2023-11-06 17:32:34 +0330

    keyName of queried model as default ownerKey;

--- src/Relations/MorphTo.php
+++ src/Relations/MorphTo.php
@@ -16,7 +16,8 @@ class MorphTo extends EloquentMorphTo
             // For belongs to relationships, which are essentially the inverse of has one
             // or has many relationships, we need to actually query on the primary key
             // of the related models matching on the foreign key that's on a parent.
-            $this->query->where($this->ownerKey, '=', $this->parent->{$this->foreignKey});
+            $this->query->where($this->ownerKey ?: $this->query->getModel()->getKeyName(), '=',
+                $this->parent->{$this->foreignKey});
         }
     }
 
