9f888ab640298968179578c0b109c8026ade568e
Mohammad Mortazavi <s.mohammad027@live.com>
2023-11-08 21:12:06 +0330

    WIP

--- src/Relations/MorphToMany.php
+++ src/Relations/MorphToMany.php
@@ -216,9 +216,9 @@ class MorphToMany extends EloquentMorphToMany
                 $id = $this->parseIds($id);
             }
 
-            if ($this->getInverse()) {
-                $query = $this->newRelatedQuery();
+            $query = $this->newRelatedQuery();
 
+            if ($this->getInverse()) {
                 $query->whereIn($this->relatedKey, (array) $id);
 
                 // Attach the new parent id to the related model.
@@ -232,8 +232,6 @@ class MorphToMany extends EloquentMorphToMany
                     ], true);
                 }
             }else{
-                $query = $this->newRelatedQuery();
-
                 $query->whereIn($this->relatedKey, (array) $id);
 
                 // Attach the new parent id to the related model.
@@ -262,7 +260,7 @@ class MorphToMany extends EloquentMorphToMany
         $query = $this->newRelatedQuery();
 
         // If associated IDs were passed to the method we will only delete those
-        // associations, otherwise all of the association ties will be broken.
+        // associations, otherwise all the association ties will be broken.
         // We'll return the numbers of affected rows when we do the deletes.
         $ids = (array) $ids;
 
