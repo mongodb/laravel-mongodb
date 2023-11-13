e9e4328a5785a279997254eea8b342aab10ec27d
Jérôme Tamarelle <jerome@tamarelle.net>
2023-11-13 14:34:02 +0100

    External package required for tests

--- tests/ExternalPackageTest.php
+++ tests/ExternalPackageTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MongoDB\Laravel\Tests;
 
-use Composer\InstalledVersions;
 use Illuminate\Http\Request;
 use MongoDB\Laravel\Tests\Models\User;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -23,16 +22,16 @@ class ExternalPackageTest extends TestCase
         User::truncate();
     }
 
+    /**
+     * Integration test with spatie/laravel-query-builder.
+     */
     public function testSpacieQueryBuilder(): void
     {
-        if (! InstalledVersions::isInstalled('spatie/laravel-query-builder')) {
-            $this->markTestSkipped('spatie/laravel-query-builder is not installed.');
-        }
-
         User::insert([
-            ['name' => 'Jane Doe', 'birthday' => '1983-09-10', 'role' => 'admin'],
-            ['name' => 'John Doe', 'birthday' => '1980-07-08', 'role' => 'admin'],
             ['name' => 'Jimmy Doe', 'birthday' => '2012-11-12', 'role' => 'user'],
+            ['name' => 'John Doe', 'birthday' => '1980-07-08', 'role' => 'admin'],
+            ['name' => 'Jane Doe', 'birthday' => '1983-09-10', 'role' => 'admin'],
+            ['name' => 'Jess Doe', 'birthday' => '2014-05-06', 'role' => 'user'],
         ]);
 
         $request = Request::create('/users', 'GET', ['filter' => ['role' => 'admin'], 'sort' => '-birthday']);
