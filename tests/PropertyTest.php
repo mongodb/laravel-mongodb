dbe13c4287f255562c9133f74f6f50f62960c0fb
Tonko Mulder <tonko@tonkomulder.nl>
2023-11-26 18:43:11 +0100

    add test for the hidden property

---
+++ tests/PropertyTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MongoDB\Laravel\Tests\Eloquent;
+
+use MongoDB\Laravel\Tests\Models\Animal;
+use MongoDB\Laravel\Tests\Models\HiddenAnimal;
+use MongoDB\Laravel\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+use function assert;
+
+final class PropertyTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Animal::truncate();
+        HiddenAnimal::truncate();
+    }
+
+    #[Test]
+    public function canHideCertainProperties(): void
+    {
+        Animal::create([
+            'name' => 'Lion',
+            'country' => 'Central Africa',
+            'can_be_eaten' => false,
+        ]);
+        HiddenAnimal::create([
+            'name' => 'Sheep',
+            'country' => 'Ireland',
+            'can_be_eaten' => true,
+        ]);
+
+        $animal = Animal::sole();
+        assert($animal instanceof Animal);
+        self::assertSame('Central Africa', $animal->country);
+        self::assertFalse($animal->can_be_eaten);
+
+        self::assertArrayHasKey('name', $animal->toArray());
+        self::assertArrayHasKey('country', $animal->toArray());
+        self::assertArrayHasKey('can_be_eaten', $animal->toArray());
+
+        $hiddenAnimal = HiddenAnimal::sole();
+        assert($hiddenAnimal instanceof HiddenAnimal);
+        self::assertSame('Ireland', $hiddenAnimal->country);
+        self::assertTrue($hiddenAnimal->can_be_eaten);
+
+        self::assertArrayHasKey('name', $hiddenAnimal->toArray());
+        self::assertArrayNotHasKey('country', $hiddenAnimal->toArray(), 'the country column should be hidden');
+        self::assertArrayHasKey('can_be_eaten', $hiddenAnimal->toArray());
+    }
+}
