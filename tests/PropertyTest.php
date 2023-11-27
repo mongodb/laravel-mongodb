8ecaf24cbd433aa80b827215bb753f21cfe2d418
Tonko Mulder <Treggats@users.noreply.github.com>
2023-11-27 13:51:52 +0100

    remove usage of Animal class

--- tests/PropertyTest.php
+++ tests/PropertyTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MongoDB\Laravel\Tests\Eloquent;
 
-use MongoDB\Laravel\Tests\Models\Animal;
 use MongoDB\Laravel\Tests\Models\HiddenAnimal;
 use MongoDB\Laravel\Tests\TestCase;
 
@@ -16,31 +15,16 @@ final class PropertyTest extends TestCase
     {
         parent::setUp();
 
-        Animal::truncate();
         HiddenAnimal::truncate();
     }
 
     public function testCanHideCertainProperties(): void
     {
-        Animal::create([
-            'name' => 'Lion',
-            'country' => 'Central Africa',
-            'can_be_eaten' => false,
-        ]);
         HiddenAnimal::create([
             'name' => 'Sheep',
             'country' => 'Ireland',
             'can_be_eaten' => true,
         ]);
-
-        $animal = Animal::sole();
-        assert($animal instanceof Animal);
-        self::assertSame('Central Africa', $animal->country);
-        self::assertFalse($animal->can_be_eaten);
-
-        self::assertArrayHasKey('name', $animal->toArray());
-        self::assertArrayHasKey('country', $animal->toArray());
-        self::assertArrayHasKey('can_be_eaten', $animal->toArray());
 
         $hiddenAnimal = HiddenAnimal::sole();
         assert($hiddenAnimal instanceof HiddenAnimal);
