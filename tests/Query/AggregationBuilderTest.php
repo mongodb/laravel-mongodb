55f05f40c43f1e5bd3ee5e23eff51ce7f5f79581
Jérôme Tamarelle <jerome@tamarelle.net>
2024-03-11 16:23:12 +0100

    Update aggregation example for mongodb 4.4 support

--- tests/Query/AggregationBuilderTest.php
+++ tests/Query/AggregationBuilderTest.php
@@ -12,13 +12,9 @@ use MongoDB\BSON\UTCDateTime;
 use MongoDB\Builder\BuilderEncoder;
 use MongoDB\Builder\Expression;
 use MongoDB\Builder\Type\Sort;
-use MongoDB\Builder\Type\TimeUnit;
-use MongoDB\Builder\Variable;
 use MongoDB\Laravel\Query\AggregationBuilder;
 use MongoDB\Laravel\Tests\Models\User;
 use MongoDB\Laravel\Tests\TestCase;
-
-use function assert;
 
 class AggregationBuilderTest extends TestCase
 {
@@ -39,16 +35,17 @@ class AggregationBuilderTest extends TestCase
             ->limit(10)
             ->offset(0)
             ->aggregate();
-        assert($pipeline instanceof AggregationBuilder);
+
+        $this->assertInstanceOf(AggregationBuilder::class, $pipeline);
+
         $pipeline
             ->addFields(
-                age: Expression::dateDiff(
-                    startDate: Expression::dateFieldPath('birthday'),
-                    endDate: Variable::now(),
-                    unit: TimeUnit::Year,
+                // Requires MongoDB 5.0+
+                year: Expression::year(
+                    Expression::dateFieldPath('birthday'),
                 ),
             )
-            ->sort(age: Sort::Desc, name: Sort::Asc)
+            ->sort(year: Sort::Desc, name: Sort::Asc)
             ->unset('birthday');
 
         // The encoder is used to convert the pipeline to a BSON document
@@ -64,30 +61,24 @@ class AggregationBuilderTest extends TestCase
                 ['$limit' => 10],
                 [
                     '$addFields' => [
-                        'age' => [
-                            '$dateDiff' => [
-                                'startDate' => '$birthday',
-                                'endDate' => '$$NOW',
-                                'unit' => 'year',
-                            ],
-                        ],
+                        'year' => ['$year' => ['date' => '$birthday']],
                     ],
                 ],
-                ['$sort' => ['age' => -1, 'name' => 1]],
+                ['$sort' => ['year' => -1, 'name' => 1]],
                 ['$unset' => ['birthday']],
             ],
         ])->toCanonicalExtendedJSON();
 
         $this->assertJsonStringEqualsJsonString($expected, $json);
 
-        // Execute the pipeline and verify the results
+        // Execute the pipeline and validate the results
         $results = $pipeline->get();
 
         $this->assertInstanceOf(Collection::class, $results);
         $this->assertCount(1, $results);
         $this->assertInstanceOf(ObjectId::class, $results->first()['_id']);
         $this->assertSame('John Doe', $results->first()['name']);
-        $this->assertIsInt($results->first()['age']);
+        $this->assertIsInt($results->first()['year']);
         $this->assertArrayNotHasKey('birthday', $results->first());
     }
 }
