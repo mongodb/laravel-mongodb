d4a981880e0ded6f3d9ceb608372fb379813364c
Chris Cho <chris.cho@10gen.com>
2024-03-25 16:56:02 -0400

    DOCSP-37057 eloquent schema builder (#2776)
    * DOCSP-37057: Eloquent schema builder

---
+++ docs/includes/schema-builder/spaceports_migration.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use MongoDB\Laravel\Schema\Blueprint;
+
+return new class extends Migration
+{
+    protected $connection = 'mongodb';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // begin create geospatial index
+        Schema::create('spaceports', function (Blueprint $collection) {
+            $collection->geospatial('launchpad_location', '2dsphere');
+            $collection->geospatial('runway_location', '2d');
+        });
+        // end create geospatial index
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('spaceports');
+    }
+};
