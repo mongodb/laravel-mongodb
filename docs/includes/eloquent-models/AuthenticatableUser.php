434d1b89269cf6c89de8ff9f42c640d8130b426d
Chris Cho <chris.cho@10gen.com>
2024-03-25 17:01:20 -0400

    DOCSP-35964 eloquent models standardization (#2726)
    * DOCSP-35964: Eloquent Models section

---
+++ docs/includes/eloquent-models/AuthenticatableUser.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+}
---
+++ docs/includes/eloquent-models/PlanetMassAssignment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class Planet extends Model
+{
+    protected $fillable = [
+        'name',
+        'gravitational_force',
+        'diameter',
+        'moons',
+    ];
+}
