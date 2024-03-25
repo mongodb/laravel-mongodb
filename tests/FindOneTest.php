c6c6e8f2d7ee7d8c57efd9fe5bb3485375467041
norareidy <nora.reidy@mongodb.com>
2024-03-25 16:13:55 -0400

    test file

---
+++ tests/FindOneTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Cargo;
+
+use App\Models\Movie;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Support\Facades\Schema;
+use MongoDB\Laravel\Tests\TestCase;
+
+use function assert;
+
+class UpdateOneTest extends TestCase
+{
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function updateOne(): void
+    {
+      require 'Models/Movie.php';
+    
+      // begin-find-one
+      $movie = Movie::where('directors', 'Rob Reiner')
+          ->orderBy('_id')
+          ->first();
+
+      print_r($movie);
+      // end-find-one
+      
+      // <optionally, add assertions>
+    }
+    
+    // ... <add additional test cases here>
+    
+}
