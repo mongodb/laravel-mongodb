97187cdc97856e51290b36257d49dff53c37d9e0
norareidy <nora.reidy@mongodb.com>
2024-03-25 15:57:09 -0400

    test file

---
+++ tests/Models/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'movies';
+    protected $fillable = ['title', 'year', 'runtime', 'imdb', 'plot'];
+}
+
+
---
+++ tests/UpdateOneTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Cargo;
+
+use App\Models\Movie;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Support\Facades\Schema;
+use MongoDB\Laravel\Tests\TestCase;
+
+use function assert;
+
+class UpdateOneTest extends TestCase
+{
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function updateOne(): void
+    {
+      require 'Models/Movie.php';
+    
+      // begin-update-one
+      $updates = Movie::where('title', 'Carol')
+          ->orderBy('_id')
+          ->first()
+          ->update([
+              "imdb" => [
+                  "rating" => 7.3,
+                  "votes" => 142000
+              ],
+          ]);
+    
+      echo "Updated documents: {$updates}";
+      // end-update-one
+      
+      // <optionally, add assertions>
+    }
+    
+    // ... <add additional test cases here>
+    
+}
