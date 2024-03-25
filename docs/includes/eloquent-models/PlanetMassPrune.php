76c7f4eb270f205fdc2a0d68b1fc6804ee1e44e1
Chris Cho <chris.cho@10gen.com>
2024-03-25 17:01:20 -0400

    DOCSP-35964 eloquent models standardization (#2726)
    * DOCSP-35964: Eloquent Models section

---
+++ docs/includes/eloquent-models/PlanetMassPrune.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\MassPrunable;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Planet extends Model
+{
+    use MassPrunable;
+
+    public function prunable()
+    {
+        // matches models in which the gravitational_force field contains
+        // a value greater than 0.5
+        return static::where('gravitational_force', '>', 0.5);
+    }
+}
