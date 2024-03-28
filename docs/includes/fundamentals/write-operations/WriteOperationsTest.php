a5a75ca1474cd2e4e1725d0b5f379d13c7455259
Chris Cho <chris.cho@mongodb.com>
2024-03-28 17:38:57 -0400

    add examples

---
+++ docs/includes/fundamentals/write-operations/WriteOperationsTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
+use MongoDB\Laravel\Collection;
+use MongoDB\Laravel\Tests\TestCase;
+use App\Models\Concert;
+use Carbon\Carbon;
+
+class WriteOperationsTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testModelInsert(): void
+    {
+        // <optionally, add code here to clean the database/collection>
+
+        require_once __DIR__ . '/Concert.php';
+
+        Concert::truncate();
+
+        // begin model insert one
+        $concert = new Concert();
+        $concert->performer = 'Mitsuko Uchida';
+        $concert->venue = 'Carnegie Hall';
+        $concert->performanceDate = Carbon::create(2024, 4, 1, 20, 0, 0, 'EST');
+        $concert->save();
+        // end model insert one
+
+        $this->assertNotNull($concert);
+
+        $result = Concert::first();
+        $this->assertInstanceOf(Concert::class, $result);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testModelInsertMassAssign(): void
+    {
+        // <optionally, add code here to clean the database/collection>
+
+        require_once __DIR__ . '/Concert.php';
+
+        Concert::truncate();
+
+        // begin model insert one mass assign
+        $insertResult = Concert::create([
+            'performer' => 'The Rolling Stones',
+            'venue' => 'Soldier Field',
+            'performanceDate' => Carbon::create(2024, 6, 30, 20, 0, 0, 'CDT')
+        ]);
+        // end model insert one mass assign
+
+        $this->assertNotNull($insertResult);
+
+        $result = Concert::first();
+        $this->assertInstanceOf(Concert::class, $result);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testModelInsertMany(): void
+    {
+        // <optionally, add code here to clean the database/collection>
+
+        require_once __DIR__ . '/Concert.php';
+
+        Concert::truncate();
+
+        // begin model insert many
+        $data = [
+            [
+                'performer' => 'Brad Mehldau',
+                'venue' => 'Philharmonie de Paris',
+                'performanceDate' => Carbon::create(2025, 2, 12, 20, 0, 0, 'CET')
+            ],
+            [
+                'performer' => 'Billy Joel',
+                'venue' => 'Madison Square Garden',
+                'performanceDate' => Carbon::create(2024, 7, 25, 19, 0, 0, 'EST')
+            ]
+        ];
+        Concert::insert($data);
+        // end model insert many
+        $results = Concert::get();
+
+        $this->assertEquals(2, count($results));
+    }
+}
