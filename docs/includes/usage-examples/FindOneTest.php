bf98e984fb06ad712a745451f5676011fcae2eca
Jérôme Tamarelle <jerome@tamarelle.net>
2024-04-02 23:53:08 +0200

    Update example test

--- docs/includes/usage-examples/FindOneTest.php
+++ docs/includes/usage-examples/FindOneTest.php
@@ -4,29 +4,30 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\Movie;
+use App\Models\UsageExamples\Movie;
 use MongoDB\Laravel\Tests\TestCase;
-
-use function print_r;
 
 class FindOneTest extends TestCase
 {
-    /**
-     * @runInSeparateProcess
-     * @preserveGlobalState disabled
-     */
     public function testFindOne(): void
     {
         require_once __DIR__ . '/Movie.php';
 
-      // begin-find-one
+        Movie::truncate();
+        Movie::insert([
+            ['title' => 'The Shawshank Redemption', 'directors' => ['Frank Darabont', 'Rob Reiner']],
+        ]);
+
+        // begin-find-one
         $movie = Movie::where('directors', 'Rob Reiner')
           ->orderBy('_id')
           ->first();
 
-        print_r($movie->toJson());
-      // end-find-one
+        echo $movie->toJson();
+        // end-find-one
 
-        $this->expectNotToPerformAssertions();
+        $this->assertInstanceOf(Movie::class, $movie);
+
+        $this->expectOutputRegex('^{"_id":"[a-z0-9]{24}","title":"The Shawshank Redemption","directors":["Frank Darabont","Rob Reiner"]}$');
     }
 }
--- docs/includes/usage-examples/Movie.php
+++ docs/includes/usage-examples/Movie.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\UsageExamples;
 
 use MongoDB\Laravel\Eloquent\Model;
 
