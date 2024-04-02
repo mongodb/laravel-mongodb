2c339bd9890b2d9e3903c2554f32b08241b40760
Chris Cho <chris.cho@mongodb.com>
2024-04-02 14:38:29 -0400

    tweaks

--- docs/includes/fundamentals/write-operations/WriteOperationsTest.php
+++ docs/includes/fundamentals/write-operations/WriteOperationsTest.php
@@ -243,8 +243,7 @@ class WriteOperationsTest extends TestCase
         Concert::where(['performer' => 'Jon Batiste', 'venue' => 'Radio City Music Hall'])
             ->update(
                 ['genres' => ['R&B', 'soul'], 'ticketsSold' => 4000],
-                ['upsert' => true],
-            );
+                ['upsert' => true]);
         // end model upsert
 
         $result = Concert::first();
