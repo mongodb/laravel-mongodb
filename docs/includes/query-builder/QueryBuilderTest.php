e3708ff235b7b39e8d450ad34c116d80ed0e9c5d
Jérôme Tamarelle <jerome@tamarelle.net>
2024-04-05 11:34:40 +0200

    Update tests with data

--- docs/includes/query-builder/QueryBuilderTest.php
+++ docs/includes/query-builder/QueryBuilderTest.php
@@ -5,33 +5,61 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Database\Query\Builder;
+use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Support\Facades\DB;
 use MongoDB\BSON\Regex;
 use MongoDB\Laravel\Collection;
 use MongoDB\Laravel\Tests\TestCase;
 
+use function file_get_contents;
+use function json_decode;
+
 class QueryBuilderTest extends TestCase
 {
-    /**
-     * @runInSeparateProcess
-     * @preserveGlobalState disabled
-     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $db = DB::connection('mongodb');
+        $db->collection('movies')
+            ->insert(json_decode(file_get_contents(__DIR__ . '/sample_mflix.movies.json'), true));
+    }
+
+    protected function importTheaters(): void
+    {
+        $db = DB::connection('mongodb');
+
+        $db->collection('theaters')
+            ->insert(json_decode(file_get_contents(__DIR__ . '/sample_mflix.theaters.json'), true));
+
+        $db->collection('theaters')
+            ->raw()
+            ->createIndex(['location.geo' => '2dsphere']);
+    }
+
+    protected function tearDown(): void
+    {
+        $db = DB::connection('mongodb');
+        $db->collection('movies')->raw()->drop();
+        $db->collection('theaters')->raw()->drop();
+
+        parent::tearDown();
+    }
+
     public function testWhere(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query where
         $result = DB::connection('mongodb')
             ->collection('movies')
             ->where('imdb.rating', 9.3)
             ->get();
         // end query where
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testOrWhere(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query orWhere
         $result = DB::connection('mongodb')
             ->collection('movies')
@@ -39,12 +67,12 @@ class QueryBuilderTest extends TestCase
             ->orWhere('title', 'Back to the Future')
             ->get();
         // end query orWhere
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testAndWhere(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query andWhere
         $result = DB::connection('mongodb')
             ->collection('movies')
@@ -52,24 +80,24 @@ class QueryBuilderTest extends TestCase
             ->where('year', '<', 1940)
             ->get();
         // end query andWhere
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testWhereNot(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query whereNot
         $result = DB::connection('mongodb')
             ->collection('movies')
             ->whereNot('imdb.rating', '>', 2)
             ->get();
         // end query whereNot
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testNestedLogical(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query nestedLogical
         $result = DB::connection('mongodb')
             ->collection('movies')
@@ -80,80 +108,80 @@ class QueryBuilderTest extends TestCase
                     ->orWhere('year', 1996);
             })->get();
         // end query nestedLogical
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testWhereBetween(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query whereBetween
         $result = DB::connection('mongodb')
             ->collection('movies')
             ->whereBetween('imdb.rating', [9, 9.5])
             ->get();
         // end query whereBetween
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testWhereNull(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query whereNull
         $result = DB::connection('mongodb')
             ->collection('movies')
             ->whereNull('runtime')
             ->get();
         // end query whereNull
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testWhereIn(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query whereIn
         $result = DB::collection('movies')
             ->whereIn('title', ['Toy Story', 'Shrek 2', 'Johnny English'])
             ->get();
         // end query whereIn
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testWhereDate(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query whereDate
         $result = DB::connection('mongodb')
             ->collection('movies')
             ->whereDate('released', '2010-1-15')
             ->get();
         // end query whereDate
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testLike(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query like
         $result = DB::collection('movies')
             ->where('title', 'like', '%spider_man%')
             ->get();
         // end query like
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testDistinct(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query distinct
         $result = DB::collection('movies')
             ->distinct('year')->get();
         // end query distinct
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testGroupBy(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query groupBy
         $result = DB::collection('movies')
            ->where('rated', 'G')
@@ -161,85 +189,86 @@ class QueryBuilderTest extends TestCase
            ->orderBy('runtime', 'asc')
            ->get(['title']);
         // end query groupBy
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testAggCount(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin aggregation count
         $result = DB::collection('movies')
             ->count();
         // end aggregation count
+
+        $this->assertIsInt($result);
     }
 
     public function testAggMax(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin aggregation max
         $result = DB::collection('movies')
             ->max('runtime');
         // end aggregation max
+
+        $this->assertIsInt($result);
     }
 
     public function testAggMin(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin aggregation min
         $result = DB::collection('movies')
             ->min('year');
         // end aggregation min
+
+        $this->assertIsInt($result);
     }
 
     public function testAggAvg(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin aggregation avg
         $result = DB::collection('movies')
             ->avg('imdb.rating');
+            //->avg('year');
         // end aggregation avg
+
+        $this->assertIsFloat($result);
     }
 
     public function testAggSum(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin aggregation sum
         $result = DB::collection('movies')
             ->sum('imdb.votes');
         // end aggregation sum
+
+        $this->assertIsInt($result);
     }
 
     public function testAggWithFilter(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin aggregation with filter
         $result = DB::collection('movies')
             ->where('year', '>', 2000)
             ->avg('imdb.rating');
         // end aggregation with filter
+
+        $this->assertIsFloat($result);
     }
 
     public function testOrderBy(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query orderBy
         $result = DB::collection('movies')
             ->where('title', 'like', 'back to the future%')
             ->orderBy('imdb.rating', 'desc')
             ->get();
         // end query orderBy
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testSkip(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query skip
         $result = DB::collection('movies')
             ->where('title', 'like', 'star trek%')
@@ -247,12 +276,12 @@ class QueryBuilderTest extends TestCase
             ->skip(4)
             ->get();
         // end query skip
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testProjection(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query projection
         $result = DB::collection('movies')
             ->where('imdb.rating', '>', 8.5)
@@ -262,12 +291,12 @@ class QueryBuilderTest extends TestCase
             ])
             ->get();
         // end query projection
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testProjectionWithPagination(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query projection with pagination
         $resultsPerPage = 15;
         $projectionFields = ['title', 'runtime', 'imdb.rating'];
@@ -276,78 +305,78 @@ class QueryBuilderTest extends TestCase
             ->orderBy('imdb.votes', 'desc')
             ->paginate($resultsPerPage, $projectionFields);
         // end query projection with pagination
+
+        $this->assertInstanceOf(AbstractPaginator::class, $result);
     }
 
     public function testExists(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query exists
         $result = DB::collection('movies')
             ->exists('random_review', true);
         // end query exists
+
+        $this->assertIsBool($result);
     }
 
     public function testAll(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query all
         $result = DB::collection('movies')
             ->where('movies', 'all', ['title', 'rated', 'imdb.rating'])
             ->get();
         // end query all
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testSize(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query size
         $result = DB::collection('movies')
             ->where('directors', 'size', 5)
             ->get();
         // end query size
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testType(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query type
         $result = DB::collection('movies')
             ->where('released', 'type', 4)
             ->get();
         // end query type
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testMod(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query modulo
         $result = DB::collection('movies')
             ->where('year', 'mod', [2, 0])
             ->get();
         // end query modulo
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testWhereRegex(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query whereRegex
         $result = DB::connection('mongodb')
             ->collection('movies')
             ->where('title', 'REGEX', new Regex('^the lord of .*', 'i'))
             ->get();
         // end query whereRegex
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testWhereRaw(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query raw
         $result = DB::collection('movies')
             ->whereRaw([
@@ -358,35 +387,38 @@ class QueryBuilderTest extends TestCase
                 ],
             ])->get();
         // end query raw
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testElemMatch(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query elemMatch
         $result = DB::collection('movies')
             ->where('writers', 'elemMatch', ['$in' => ['Maya Forbes', 'Eric Roth']])
             ->get();
         // end query elemMatch
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testCursorTimeout(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query cursor timeout
         $result = DB::collection('movies')
             ->timeout(2) // value in seconds
             ->where('year', 2001)
             ->get();
         // end query cursor timeout
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $result);
     }
 
     public function testNear(): void
     {
-        // <optionally, add code here to clean the database/collection>
-        // begin query near
+        $this->importTheaters();
+
+       // begin query near
         $results = DB::collection('theaters')
             ->where('location.geo', 'near', [
                 '$geometry' => [
@@ -399,12 +431,12 @@ class QueryBuilderTest extends TestCase
                 '$maxDistance' => 50,
             ])->get();
         // end query near
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $results);
     }
 
     public function testGeoWithin(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query geoWithin
         $results = DB::collection('theaters')
             ->where('location.geo', 'geoWithin', [
@@ -421,12 +453,12 @@ class QueryBuilderTest extends TestCase
                 ],
             ])->get();
         // end query geoWithin
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $results);
     }
 
     public function testGeoIntersects(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin query geoIntersects
         $results = DB::collection('theaters')
             ->where('location.geo', 'geoIntersects', [
@@ -439,11 +471,13 @@ class QueryBuilderTest extends TestCase
                 ],
             ])->get();
         // end query geoIntersects
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $results);
     }
 
     public function testGeoNear(): void
     {
-        // <optionally, add code here to clean the database/collection>
+        $this->importTheaters();
 
         // begin query geoNear
         $results = DB::collection('theaters')->raw(
@@ -463,14 +497,15 @@ class QueryBuilderTest extends TestCase
                     ],
                 ]);
             },
-        );
+        )->toArray();
         // end query geoNear
+
+        $this->assertIsArray($results);
+        $this->assertSame(8900, $results[0]['theaterId']);
     }
 
     public function testUpsert(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin upsert
         $result = DB::collection('movies')
             ->where('title', 'Will Hunting')
@@ -483,60 +518,62 @@ class QueryBuilderTest extends TestCase
                 ['upsert' => true],
             );
         // end upsert
+
+        $this->assertIsInt($result);
     }
 
     public function testIncrement(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin increment
         $result = DB::collection('movies')
             ->where('title', 'Field of Dreams')
             ->increment('imdb.votes', 3000);
         // end increment
+
+        $this->assertIsInt($result);
     }
 
     public function testDecrement(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin decrement
         $result = DB::collection('movies')
             ->where('title', 'Sharknado')
             ->decrement('imdb.rating', 0.2);
         // end decrement
+
+        $this->assertIsInt($result);
     }
 
     public function testPush(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin push
         $result = DB::collection('movies')
             ->where('title', 'Office Space')
             ->push('cast', 'Gary Cole');
         // end push
+
+        $this->assertIsInt($result);
     }
 
     public function testPull(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin pull
         $result = DB::collection('movies')
             ->where('title', 'Iron Man')
             ->pull('genres', 'Adventure');
         // end pull
+
+        $this->assertIsInt($result);
     }
 
     public function testUnset(): void
     {
-        // <optionally, add code here to clean the database/collection>
-
         // begin unset
         $result = DB::collection('movies')
             ->where('title', 'Final Accord')
             ->unset('tomatoes.viewer');
         // end unset
+
+        $this->assertIsInt($result);
     }
 }
