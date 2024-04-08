2c5de4342c042c604ec13047c985070e1a2d8029
norareidy <nora.reidy@mongodb.com>
2024-04-08 11:56:13 -0400

    DOCSP-35977: Find multiple usage example

---
+++ docs/includes/usage-examples/FindManyTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use MongoDB\Laravel\Tests\TestCase;
+
+class FindManyTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testFindMany(): void
+    {
+        require_once __DIR__ . '/Movie.php';
+
+        Movie::truncate();
+
+        // begin-find
+        $movies = Movie::where('runtime', '>', 800)
+            ->orderBy('_id')
+            ->get();
+
+        foreach ($movies as $m) {
+            echo $m->toJson() . '<br>';
+        }
+        // end-find
+
+        $this->assertInstanceOf(Movie::class, $movie);
+    }
+}
