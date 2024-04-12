bb7af033463fb10a9a1e3dfa9a07f286d81096d5
norareidy <nora.reidy@mongodb.com>
2024-04-12 15:11:18 -0400

    DOCSP-35983: Run command usage example

---
+++ docs/includes/usage-examples/RunCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use MongoDB\Laravel\Tests\TestCase;
+
+class RunCommandTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testRunCommand(): void
+    {
+        require_once __DIR__ . '/Movie.php';
+
+        // begin-update-one
+        $cursor = DB::connection('mongodb')
+            ->command(['listCollections' => 1]);
+
+        foreach ($cursor as $coll) {
+            echo $coll['name'] . '<br>';
+        }
+        // end-update-one
+
+        $this->assertNotNull($cursor);
+        //$this->expectOutputString('movies');
+    }
+}
