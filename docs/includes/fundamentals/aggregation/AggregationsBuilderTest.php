2b62498ff894ece48751289aa88d2e15e21d8c89
Chris Cho <chris.cho@mongodb.com>
2024-04-16 15:37:24 -0400

    add custom operator factory section

--- docs/includes/fundamentals/aggregation/AggregationsBuilderTest.php
+++ docs/includes/fundamentals/aggregation/AggregationsBuilderTest.php
@@ -8,6 +8,7 @@ use DateTimeImmutable;
 use MongoDB\BSON\UTCDateTime;
 use MongoDB\Builder\Accumulator;
 use MongoDB\Builder\Expression;
+use MongoDB\Builder\Expression\YearOperator;
 use MongoDB\Builder\Query;
 use MongoDB\Builder\Type\Sort;
 use MongoDB\Laravel\Tests\Models\User;
@@ -141,6 +142,39 @@ class AggregationsBuilderTest extends TestCase
             ->sort(_id: Sort::Asc)
             ->project(profession: Expression::fieldPath('_id'), year_avg: 1, _id: 0);
         // end pipeline example
+
+        $result = $pipeline->get();
+        print_r($result->toJson());
+        $this->assertEquals(2, $result->count());
+        $this->assertNotNull($result->first()['year_avg']);
+    }
+
+    // start custom operator factory function
+    function yearFromField(string $dateFieldName): YearOperator
+    {
+        return Expression::year(
+            $dateFieldName = Expression::dateFieldPath($dateFieldName),
+        );
+    }
+
+    // end custom operator factory function
+    public function testCustomOperatorFactory(): void
+    {
+        User::truncate();
+        User::insert([
+            ['name' => 'Alda Gröndal', 'occupation' => 'engineer', 'birthday' => new UTCDateTime(new DateTimeImmutable('2002-01-01'))],
+            ['name' => 'Francois Soma', 'occupation' => 'engineer', 'birthday' => new UTCDateTime(new DateTimeImmutable('1998-02-02'))],
+            ['name' => 'Janet Doe', 'occupation' => 'designer', 'birthday' => new UTCDateTime(new DateTimeImmutable('1987-03-03'))],
+            ['name' => 'Eliud Nkosana', 'occupation' => 'engineer', 'birthday' => new UTCDateTime(new DateTimeImmutable('1984-04-04'))],
+            ['name' => 'Bran Steafan', 'occupation' => 'engineer', 'birthday' => new UTCDateTime(new DateTimeImmutable('1998-05-05'))],
+            ['name' => 'Ellis Lee', 'occupation' => 'designer', 'birthday' => new UTCDateTime(new DateTimeImmutable('1996-06-06'))],
+        ]);
+
+        // begin custom operator factory usage
+        $pipeline = User::aggregate()
+            ->addFields(birth_year: $this->yearFromField('birthday'))
+            ->project(_id:0, name: 1, birth_year: 1);
+        // end custom operator factory usage
 
         $result = $pipeline->get();
         print_r($result->toJson());
