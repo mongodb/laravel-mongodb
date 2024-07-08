d98f39690813a905079d38edfc2b3b038811c15d
norareidy <nora.reidy@mongodb.com>
2024-07-08 11:47:35 -0400

    DOCSP-38100: User authentication

---
+++ docs/includes/auth/AuthUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
