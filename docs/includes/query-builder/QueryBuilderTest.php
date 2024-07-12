30f65648a7be7cad73e070ec3560bed2e2b7ff9f
norareidy <norareidy@users.noreply.github.com>
2024-07-12 19:43:30 +0000

    apply phpcbf formatting

--- docs/includes/query-builder/QueryBuilderTest.php
+++ docs/includes/query-builder/QueryBuilderTest.php
@@ -7,10 +7,10 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Support\Facades\DB;
+use MongoDB\BSON\ObjectId;
 use MongoDB\BSON\Regex;
 use MongoDB\Laravel\Collection;
 use MongoDB\Laravel\Tests\TestCase;
-use MongoDB\BSON\ObjectId;
 
 use function file_get_contents;
 use function json_decode;
