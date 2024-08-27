fd30e09358b5d1892d42f23b14125741fb44d783
norareidy <nora.reidy@mongodb.com>
2024-08-27 13:38:41 -0400

    edits, code file

---
+++ docs/includes/auth/AuthServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use MongoDB\Laravel\Passport\AuthCode;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Passport::useAuthCodeModel(AuthCode::class);
+    }
+}
