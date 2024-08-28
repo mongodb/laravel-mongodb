130cce854357a1dcc49a3f912c2a94aaba66451d
norareidy <nora.reidy@mongodb.com>
2024-08-28 16:12:48 -0400

    JT feedback

--- docs/includes/auth/AppServiceProvider.php
+++ docs/includes/auth/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use MongoDB\Laravel\Passport\AuthCode;
+use MongoDB\Laravel\Passport\Client;
+use MongoDB\Laravel\Passport\PersonalAccessClient;
+use MongoDB\Laravel\Passport\RefreshToken;
+use MongoDB\Laravel\Passport\Token;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Passport::useAuthCodeModel(AuthCode::class);
+        Passport::useClientModel(Client::class);
+        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
+        Passport::useRefreshTokenModel(RefreshToken::class);
+        Passport::useTokenModel(Token::class);
     }
 }
