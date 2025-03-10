c53d32a53a64b03734b266679d9854cfa95b3cb9
rustagir <rea.rustagi@mongodb.com>
2025-03-10 15:21:25 -0400

    tests

--- docs/includes/fundamentals/read-operations/ReadOperationsTest.php
+++ docs/includes/fundamentals/read-operations/ReadOperationsTest.php
@@ -208,7 +208,7 @@ class ReadOperationsTest extends TestCase
 
         // end-query-log
 
-        $this->expectOutputString('{ "query": "{ \"find\" : \"movies\", \"filter\" : { \"title\" : \"Carrie\" } }", "bindings": [], "time": 35366 }{ "query": "{ \"find\" : \"movies\", \"filter\" : { \"year\" : { \"$lt\" : { \"$numberInt\" : \"2005\" } } } }", "bindings": [], "time": 31571 }{ "query": "{ \"find\" : \"movies\", \"filter\" : { \"imdb.rating\" : { \"$gt\" : { \"$numberDouble\" : \"8.5\" } } } }", "bindings": [], "time": 26469 }');
+        $this->expectOutputRegex('\{ "query": "\{ \\"find\\" : \\"movies\\", \\"filter\\" : \{ \\"title\\" : \\"Carrie\\" \} \}", "bindings": \[\], "time": [0-9]+ \}\{ "query": "\{ \\"find\\" : \\"movies\\", \\"filter\\" : \{ \\"year\\" : \{ \\"\$lt\\" : \{ \\"\$numberInt\\" : \\"2005\\" \} \} \} \}", "bindings": \[\], "time": [0-9]+ \}\{ "query": "\{ \\"find\\" : \\"movies\\", \\"filter\\" : \{ \\"imdb\.rating\\" : \{ \\"\$gt\\" : \{ \\"\$numberDouble\\" : \\"8\.5\\" \} \} \} \}", "bindings": \[\], "time": [0-9]+ \}');
         $this->assertNotNull($logs);
     }
 }
