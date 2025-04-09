8e534f0355daeacc089d16a25960e0da79d7884e
saineshmamgain <saineshmamgain@gmail.com>
2025-04-09 20:57:07 +0530

    Fix:test

--- tests/PropertyTest.php
+++ tests/PropertyTest.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace MongoDB\Laravel\Tests\Eloquent;
 
-use Carbon\CarbonImmutable;
-use Illuminate\Support\Facades\Date;
-use MongoDB\Laravel\Tests\Models\Anniversary;
 use MongoDB\Laravel\Tests\Models\HiddenAnimal;
 use MongoDB\Laravel\Tests\TestCase;
 
@@ -19,7 +16,6 @@ final class PropertyTest extends TestCase
         parent::setUp();
 
         HiddenAnimal::truncate();
-        Anniversary::truncate();
     }
 
     public function testCanHideCertainProperties(): void
@@ -38,19 +34,5 @@ final class PropertyTest extends TestCase
         self::assertArrayHasKey('name', $hiddenAnimal->toArray());
         self::assertArrayNotHasKey('country', $hiddenAnimal->toArray(), 'the country column should be hidden');
         self::assertArrayHasKey('can_be_eaten', $hiddenAnimal->toArray());
-    }
-
-    public function testCanReturnCarbonImmutableObject(): void
-    {
-        Date::use(CarbonImmutable::class);
-
-        Anniversary::create([
-            'name' => 'John',
-            'anniversary' => new CarbonImmutable('2020-01-01 00:00:00'),
-        ]);
-
-        $anniversary = Anniversary::sole();
-        assert($anniversary instanceof Anniversary);
-        self::assertInstanceOf(CarbonImmutable::class, $anniversary->anniversary);
     }
 }
