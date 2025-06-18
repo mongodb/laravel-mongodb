8d8c176957258768411dfa346aaa8d8b69419877
Pauline Vos <pauline.vos@mongodb.com>
2025-06-18 14:37:28 +0200

    Add `#[Override]` attribute to Validation namespace

--- src/Validation/DatabasePresenceVerifier.php
+++ src/Validation/DatabasePresenceVerifier.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MongoDB\Laravel\Validation;
 
 use MongoDB\BSON\Regex;
+use Override;
 
 use function array_map;
 use function implode;
@@ -12,17 +13,8 @@ use function preg_quote;
 
 class DatabasePresenceVerifier extends \Illuminate\Validation\DatabasePresenceVerifier
 {
-    /**
-     * Count the number of objects in a collection having the given value.
-     *
-     * @param string $collection
-     * @param string $column
-     * @param string $value
-     * @param int    $excludeId
-     * @param string $idColumn
-     *
-     * @return int
-     */
+    /** Count the number of objects in a collection having the given value. */
+    #[Override]
     public function getCount($collection, $column, $value, $excludeId = null, $idColumn = null, array $extra = [])
     {
         $query = $this->table($collection)->where($column, new Regex('^' . preg_quote($value) . '$', '/i'));
@@ -38,16 +30,8 @@ class DatabasePresenceVerifier extends \Illuminate\Validation\DatabasePresenceVe
         return $query->count();
     }
 
-    /**
-     * Count the number of objects in a collection with the given values.
-     *
-     * @param string $collection
-     * @param string $column
-     * @param array  $values
-     * @param array  $extra
-     *
-     * @return int
-     */
+    /** Count the number of objects in a collection with the given values. */
+    #[Override]
     public function getMultiCount($collection, $column, array $values, array $extra = [])
     {
         // Nothing can match an empty array. Return early to avoid matching an empty string.
--- src/Validation/ValidationServiceProvider.php
+++ src/Validation/ValidationServiceProvider.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace MongoDB\Laravel\Validation;
 
 use Illuminate\Validation\ValidationServiceProvider as BaseProvider;
+use Override;
 
 class ValidationServiceProvider extends BaseProvider
 {
+    #[Override]
     protected function registerPresenceVerifier()
     {
         $this->app->singleton('validation.presence', function ($app) {
