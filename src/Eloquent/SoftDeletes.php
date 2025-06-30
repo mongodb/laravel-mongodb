ece1a77f2ce9b3bc80df4ae98c8d973b2a219cce
Pauline Vos <pvos88@gmail.com>
2025-06-30 12:05:05 +0200

    Update src/Eloquent/SoftDeletes.php
    Co-authored-by: Jérôme Tamarelle <jerome@tamarelle.net>

--- src/Eloquent/SoftDeletes.php
+++ src/Eloquent/SoftDeletes.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace MongoDB\Laravel\Eloquent;
 
-/** @deprecated 6.0.0 in favor of \Illuminate\Database\Eloquent\SoftDeletes */
+use function sprintf;
+use function trigger_error;
+
+use const E_USER_DEPRECATED;
+
+trigger_error(sprintf('Since mongodb/laravel-mongodb:5.5, trait "%s" is deprecated, use "%s" instead.', SoftDeletes::class, \Illuminate\Database\Eloquent\SoftDeletes::class), E_USER_DEPRECATED);
+
+/** @deprecated since mongodb/laravel-mongodb:5.5, use \Illuminate\Database\Eloquent\SoftDeletes instead */
 trait SoftDeletes
 {
     use \Illuminate\Database\Eloquent\SoftDeletes;
